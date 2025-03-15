d35f27ce9bb32e33f62fd6de64ce5f79b009f4b6
Denver <deng37@gmail.com>
2025-03-15 19:15:58 +0700

    feat(main): Portfolio - add slider function

--- js/stylish-portfolio.js
+++ js/stylish-portfolio.js
@@ -40,6 +40,25 @@
     }
   });
 
+  // Change color of side menu
+  $(window).scroll(function() {
+      var scroll = $(window).scrollTop(); // how many pixels you've scrolled
+      var os = $('#portfolio').offset().top; // pixels to the top of div1
+      var ht = $('#portfolio').height(); // height of div1 in pixels
+      if (scroll > os){
+          $('#menu').addClass('colorBlack');
+      }
+  });
+
+  window.onscroll = function() {
+      var nav = document.getElementById('nav');
+      // if ( window.pageYOffset > 100 ) {
+      //     nav.classList.add("navbar1");
+      // } else {
+      //     nav.classList.remove("navbar1");
+      // }
+  }
+
 })(jQuery); // End of use strict
 
 // Disable Google Maps scrolling
@@ -62,3 +81,41 @@ var onMapClickHandler = function(event) {
 }
 // Enable map zooming with mouse scroll when the user clicks the map
 $('.map').on('click', onMapClickHandler);
+
+// Slider
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function plusSlidess(n) {
+  let randomIndex = Math.floor(Math.random() * 5);
+  currentSlide(randomIndex);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[slideIndex-1].style.display = "block";
+}
+
+// Keyboard arrow for next and previous slider
+$("#page-top").keydown(function(e) {
+  if (e.keyCode == 37) {
+      showSlides(slideIndex += -1);
+  } else if (e.keyCode == 39) {
+      showSlides(slideIndex += 1);
+  }
+});
