413d09abdd7988dcaada054b9d45a1ee231efcaa
Denver <deng37@gmail.com>
2025-03-18 10:32:30 +0700

    feat(main): Index - update responsive

--- js/stylish-portfolio.js
+++ js/stylish-portfolio.js
@@ -1,121 +1,39 @@
 (function($) {
-  "use strict"; // Start of use strict
-
-  // Closes the sidebar menu
-  $(".menu-toggle").click(function(e) {
-    e.preventDefault();
-    $("#sidebar-wrapper").toggleClass("active");
-    $(".menu-toggle > .fa-bars, .menu-toggle > .fa-times").toggleClass("fa-bars fa-times");
-    $(this).toggleClass("active");
-  });
-
-  // Smooth scrolling using jQuery easing
-  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
-    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-      if (target.length) {
-        $('html, body').animate({
-          scrollTop: target.offset().top
-        }, 1000, "easeInOutExpo");
-        return false;
-      }
-    }
-  });
-
-  // Closes responsive menu when a scroll trigger link is clicked
-  $('#sidebar-wrapper .js-scroll-trigger').click(function() {
-    $("#sidebar-wrapper").removeClass("active");
-    $(".menu-toggle").removeClass("active");
-    $(".menu-toggle > .fa-bars, .menu-toggle > .fa-times").toggleClass("fa-bars fa-times");
-  });
-
-  // Scroll to top button appear
-  $(document).scroll(function() {
-    var scrollDistance = $(this).scrollTop();
-    if (scrollDistance > 100) {
-      $('.scroll-to-top').fadeIn();
-    } else {
-      $('.scroll-to-top').fadeOut();
-    }
-  });
-
-  // Change color of side menu
-  $(window).scroll(function() {
-      var scroll = $(window).scrollTop(); // how many pixels you've scrolled
-      var os = $('#portfolio').offset().top; // pixels to the top of div1
-      var ht = $('#portfolio').height(); // height of div1 in pixels
-      if (scroll > os){
-          $('#menu').addClass('colorBlack');
-      }
-  });
-
-  window.onscroll = function() {
-      var nav = document.getElementById('nav');
-      // if ( window.pageYOffset > 100 ) {
-      //     nav.classList.add("navbar1");
-      // } else {
-      //     nav.classList.remove("navbar1");
-      // }
-  }
-
 })(jQuery); // End of use strict
 
-// Disable Google Maps scrolling
-// See http://stackoverflow.com/a/25904582/1607849
-// Disable scroll zooming and bind back the click event
-var onMapMouseleaveHandler = function(event) {
-  var that = $(this);
-  that.on('click', onMapClickHandler);
-  that.off('mouseleave', onMapMouseleaveHandler);
-  that.find('iframe').css("pointer-events", "none");
-}
-var onMapClickHandler = function(event) {
-  var that = $(this);
-  // Disable the click handler until the user leaves the map area
-  that.off('click', onMapClickHandler);
-  // Enable scrolling zoom
-  that.find('iframe').css("pointer-events", "auto");
-  // Handle the mouse leave event
-  that.on('mouseleave', onMapMouseleaveHandler);
-}
-// Enable map zooming with mouse scroll when the user clicks the map
-$('.map').on('click', onMapClickHandler);
+  // Slider
+  let slideIndex = 1;
+  showSlides(slideIndex);
 
-// Slider
-let slideIndex = 1;
-showSlides(slideIndex);
-
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function plusSlidess(n) {
-  let randomIndex = Math.floor(Math.random() * 5);
-  currentSlide(randomIndex);
-}
-
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
+  function plusSlides(n) {
+    showSlides(slideIndex += n);
   }
-  slides[slideIndex-1].style.display = "block";
-}
 
-// Keyboard arrow for next and previous slider
-$("#page-top").keydown(function(e) {
-  if (e.keyCode == 37) {
-      showSlides(slideIndex += -1);
-  } else if (e.keyCode == 39) {
-      showSlides(slideIndex += 1);
+  function currentSlide(n) {
+    showSlides(slideIndex = n);
   }
-});
+
+  function plusSlidess(n) {
+    let randomIndex = Math.floor(Math.random() * 5);
+    currentSlide(randomIndex);
+  }
+
+  function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+    slides[slideIndex-1].style.display = "block";
+  }
+
+    // Keyboard arrow for next and previous slider
+    $("#page-top").keydown(function(e) {
+        if (e.keyCode == 37) {
+            showSlides(slideIndex += -1);
+        } else if (e.keyCode == 39) {
+            showSlides(slideIndex += 1);
+        }
+    });
